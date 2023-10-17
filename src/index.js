d873bc7ba66aa156d09edfe5377afa54f171fc4e
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-17 14:24:36 -0500

    Fetch api and created nodes

--- src/index.js
+++ src/index.js
@@ -4,3 +4,29 @@
  **/
 
 console.log('Happy hacking :)')
+
+const API = 'https://platzi-avo.vercel.app/api/avo';
+//1-connects with the server
+window.fetch(API)
+//2-converts into json  
+.then(response => response.json())
+//3-manipulates the data  
+.then(dataJSON => {
+  const fetchItems = [];
+  
+    dataJSON.data.forEach(e => {
+      const h2 = document.createElement('h2');
+
+      const img = document.createElement('img');
+
+      const span = document.createElement('span');
+
+      const div = document.createElement('div');
+
+
+      div.append(h2,img,span);
+      fetchItems.push(div);
+    });
+    console.log(fetchItems);
+    document.body.append(...fetchItems);
+  })
