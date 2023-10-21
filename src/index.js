7df3403fe8aaecf3cb3ee335e7d12158b2e5b216
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-21 14:26:23 -0500

    Imported styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import './styles/styles.css';
 
 console.log('Happy hacking :)')
 const app = document.getElementById('app');
