49f4613060ce9502a38f6da7790877c12c8b87c7
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-21 15:47:23 -0500

    Added terser-webpack-plugin in order to minimize index.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin'); 
 
 
 module.exports = {
@@ -43,8 +44,12 @@ module.exports = {
     new CleanWebpackPlugin()
   ],
   optimization:{
+    minimize: true,
     minimizer:[
-      new CssMinimizerWebpackPlugin()
+      new TerserWebpackPlugin(),
+      new CssMinimizerWebpackPlugin(),
+
+
     ]
   }
 }
